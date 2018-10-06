49b598ed1edbf1af6a227bbaf7e93a773ca680e3
Aman <amanyadavlko2502@gmail.com>
2018-10-06 12:25:42 +0530

    Added rand()

--- time-table/src/generator/generator.js
+++ time-table/src/generator/generator.js
@@ -1,6 +1,66 @@
 // Function to generate the best possible timetable.
 
-const instaces = [{
+const crypto = require("crypto");
+
+const rand = async (slots, count) => {
+	
+	let i;
+	let viableDays = [];
+	for(i in slots){
+		if (slots[i].length > count){
+			viableDays.push(i);
+		}
+	}
+	
+	if(viableDays.length == 0)
+		return null; 
+
+	let buff = crypto.randomBytes(2);
+	let n = parseInt(buff.toString('hex'),16)
+	console.log(n);	
+
+	let index =  n % (viableDays.length);
+	let day = viableDays[index]
+
+
+	buff = crypto.randomBytes(2);
+	n = parseInt(buff.toString('hex'),16)
+	console.log(n);
+
+	let slot = n % slots[day].length;
+
+	console.log(day, slot)
+}
+
+const generate = async (instaces, givenSlots, teachers, sections) => {
+	
+	let secInstances  = {};	
+	let i,j,k;
+	for (i in sections){
+		secInstances[sections[i]] = [];
+	}
+
+	for (i in sections){
+		for(j in instaces){
+			for(let k in instaces[j].sections){ 
+				if(instaces[j].sections[k] == sections[i]){
+					secInstances[sections[i]].push(instaces[j]);
+				}
+			}	
+		}
+	}
+	
+	//console.log(secInstances);
+	let TT = [];
+	let secTT = [];
+	let techerTT = [];
+
+	for(i in secInstances){
+					
+	}
+}
+/*
+generate([{
 	teacher: "T1",
 	sections: ["12A"],
 	subject: "English",
@@ -48,33 +108,5 @@ const instaces = [{
 	subject: "Science",
 	numLectures: "4",
 	numLabs: null
-}]
-
-const sections = ["12A", "12B"];
-const teachers = ["T1", "T2", "T3", "T4", "T5", "T6"];
-const slots = [8,8,8,8,8,8,5];
-
-
-//const generate = async (instaces, slots, teachers, sections) => {
-	
-	let secTeachers  = {};	
-	let i,j,k;
-	for (i in sections){
-		secTeachers[sections[i]] = [];
-	}
-
-	console.log(secTeachers);
-	
-	for (i in sections){
-		for(j in instaces){
-			for(let k in instaces[j].sections){ 
-				if(instaces[j].sections[k] == sections[i]){
-					secTeachers[sections[i]].push(instaces[j].teacher);
-				}
-			}	
-		}
-	}
-
-	console.log(secTeachers)
-//}
-
+}], [8,8,8,8,8,8,5], ["T1", "T2", "T3", "T4", "T5", "T6"], ["12A", "12B"]); */
+rand([[1,2,3,4], [1,2,3,4], [1,2,3,4]], 2)
