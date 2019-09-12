ae2b77909feab77609f4b1c7aaba2ab03133ff64
Mayank Pathela <mp.pathela@gmail.com>
2019-09-13 03:02:31 +0530

    Few Changes

--- time-table/client/src/components/About.js
+++ time-table/client/src/components/About.js
@@ -10,6 +10,90 @@ function About() {
         <Row>
           <Col sm="4">
             {/* <Card className="mb-4"> */}
+            <CardImg src="assets/mayank.jpg" />
+            <CardBody>
+              <CardTitle>
+                <strong class="name">Mayank Pathela</strong>
+                <br />
+                <span class="social-media">
+                  <a
+                    href="https://www.linkedin.com/in/mayank-pathela-1a165b154/"
+                    title="LinkedIn"
+                  >
+                    <i class="fa fa-linkedin-square" />
+                  </a>{" "}
+                  &nbsp;
+                  <a href="https://twitter.com/StarkBlaze01" title="Twitter">
+                    <i class="fa fa-twitter" />
+                  </a>{" "}
+                  &nbsp;
+                  <a href="https://github.com/starkblaze01" title="GitHub">
+                    <i class="fa fa-github" />
+                  </a>
+                </span>
+              </CardTitle>
+            </CardBody>
+            {/* </Card> */}
+          </Col>
+
+          <Col sm="4">
+            {/* <Card className="mb-4"> */}
+            <CardImg src="assets/kirtika.png" />
+            <CardBody>
+              <CardTitle>
+                <strong class="name">Kirtika Singhal</strong>
+                <br />
+                <span class="social-media">
+                  <a
+                    href="https://www.linkedin.com/in/kirtika-singhal-30371416a/"
+                    title="LinkedIn"
+                  >
+                    <i class="fa fa-linkedin-square" />
+                  </a>{" "}
+                  &nbsp;
+                  <a href="https://twitter.com/Kirtika_28" title="Twitter">
+                    <i class="fa fa-twitter" />
+                  </a>{" "}
+                  &nbsp;
+                  <a href="https://github.com/singhalkirtika" title="GitHub">
+                    <i class="fa fa-github" />
+                  </a>
+                </span>
+              </CardTitle>
+            </CardBody>
+            {/* </Card> */}
+          </Col>
+          <Col sm="4">
+            {/* <Card className="mb-4"> */}
+            <CardImg src="assets/daksh.jpg" />
+            <CardBody>
+              <CardTitle>
+                <strong class="name">Daksh Gondaliya</strong>
+                <br />
+                <span class="social-media">
+                  <a
+                    href="https://www.linkedin.com/in/daksh-gondaliya-64625615b/"
+                    title="LinkedIn"
+                  >
+                    <i class="fa fa-linkedin-square" />
+                  </a>{" "}
+                  &nbsp;
+                  <a href="https://twitter.com/gondaliyadaksh" title="Twitter">
+                    <i class="fa fa-twitter" />
+                  </a>{" "}
+                  &nbsp;
+                  <a href="https://github.com/DakshGondaliya" title="GitHub">
+                    <i class="fa fa-github" />
+                  </a>
+                </span>
+              </CardTitle>
+            </CardBody>
+            {/* </Card> */}
+          </Col>          
+        </Row>
+        <Row>
+          <Col sm="4">
+            {/* <Card className="mb-4"> */}
             <CardImg src="assets/aman.jpg" />
             <CardBody>
               <CardTitle>
@@ -64,93 +148,6 @@ function About() {
             </CardBody>
             {/* </Card> */}
           </Col>
-
-          <Col sm="4">
-            {/* <Card className="mb-4"> */}
-            <CardImg src="assets/daksh.jpg" />
-            <CardBody>
-              <CardTitle>
-                <strong class="name">Daksh Gondaliya</strong>
-                <br />
-                <span class="social-media">
-                  <a
-                    href="https://www.linkedin.com/in/daksh-gondaliya-64625615b/"
-                    title="LinkedIn"
-                  >
-                    <i class="fa fa-linkedin-square" />
-                  </a>{" "}
-                  &nbsp;
-                  <a href="https://twitter.com/gondaliyadaksh" title="Twitter">
-                    <i class="fa fa-twitter" />
-                  </a>{" "}
-                  &nbsp;
-                  <a href="https://github.com/DakshGondaliya" title="GitHub">
-                    <i class="fa fa-github" />
-                  </a>
-                </span>
-              </CardTitle>
-            </CardBody>
-            {/* </Card> */}
-          </Col>
-        </Row>
-
-        <Row>
-          <Col sm="4">
-            {/* <Card className="mb-4"> */}
-            <CardImg src="assets/mayank.jpg" />
-            <CardBody>
-              <CardTitle>
-                <strong class="name">Mayank Pathela</strong>
-                <br />
-                <span class="social-media">
-                  <a
-                    href="https://www.linkedin.com/in/mayank-pathela-1a165b154/"
-                    title="LinkedIn"
-                  >
-                    <i class="fa fa-linkedin-square" />
-                  </a>{" "}
-                  &nbsp;
-                  <a href="https://twitter.com/StarkBlaze01" title="Twitter">
-                    <i class="fa fa-twitter" />
-                  </a>{" "}
-                  &nbsp;
-                  <a href="https://github.com/starkblaze01" title="GitHub">
-                    <i class="fa fa-github" />
-                  </a>
-                </span>
-              </CardTitle>
-            </CardBody>
-            {/* </Card> */}
-          </Col>
-
-          <Col sm="4">
-            {/* <Card className="mb-4"> */}
-            <CardImg src="assets/kirtika.png" />
-            <CardBody>
-              <CardTitle>
-                <strong class="name">Kirtika Singhal</strong>
-                <br />
-                <span class="social-media">
-                  <a
-                    href="https://www.linkedin.com/in/kirtika-singhal-30371416a/"
-                    title="LinkedIn"
-                  >
-                    <i class="fa fa-linkedin-square" />
-                  </a>{" "}
-                  &nbsp;
-                  <a href="https://twitter.com/Kirtika_28" title="Twitter">
-                    <i class="fa fa-twitter" />
-                  </a>{" "}
-                  &nbsp;
-                  <a href="https://github.com/singhalkirtika" title="GitHub">
-                    <i class="fa fa-github" />
-                  </a>
-                </span>
-              </CardTitle>
-            </CardBody>
-            {/* </Card> */}
-          </Col>
-
           <Col sm="4">
             {/* <Card className="mb-4"> */}
             <CardImg src="assets/parmeshwar.jpg" />
@@ -182,6 +179,8 @@ function About() {
             {/* </Card> */}
           </Col>
         </Row>
+
+        
       </div>
     </div>
   );
