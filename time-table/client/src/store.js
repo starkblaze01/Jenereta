b1a9fe8a3cfd6dbd6c2a8f8770635a12dff6fdec
Mayank Pathela <mp.pathela@gmail.com>
2019-09-13 03:14:02 +0530

    bug fix

--- time-table/client/src/store.js
+++ time-table/client/src/store.js
@@ -9,8 +9,9 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware) ,
-    process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
+    applyMiddleware(...middleware) 
+    // ,
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 export default store;
